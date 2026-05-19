d5474e0dc3eee97fcde7944034af9441d7eff96e
Yuan Tong <tongyuan200097@gmail.com>
2026-05-19 17:15:30 +0800

    wallpaper: Update display info cache on display change

--- services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
+++ services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
@@ -543,6 +543,12 @@ public class WallpaperManagerService extends IWallpaperManager.Stub
 
         @Override
         public void onDisplayChanged(int displayId) {
+            synchronized (mLock) {
+                if (DEBUG) {
+                    Slog.d(TAG, "Refreshing wallpaper display data for display " + displayId);
+                }
+                mWallpaperDisplayHelper.refreshDisplayData(displayId);
+            }
         }
     };
 
